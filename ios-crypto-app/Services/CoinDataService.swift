30903b158a28de59e459d723313c7585c7b49aa7
hridayanp <hridayanphukan123@gmail.com>
2025-03-20 13:19:25 +0530

    added reusable networking manager

--- ios-crypto-app/Services/CoinDataService.swift
+++ ios-crypto-app/Services/CoinDataService.swift
@@ -22,25 +22,9 @@ class CoinDataService {
         guard let API_URL = URL(string: "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=250&page=1&sparkline=true&price_change_percentage=24h")
         else { return }
         
-        coinSubscription = URLSession.shared.dataTaskPublisher(for: API_URL)
-            .subscribe(on: DispatchQueue.global(qos: .default))
-            .tryMap { output -> Data in
-                guard let response = output.response as? HTTPURLResponse, response.statusCode >= 200 && response.statusCode < 300 else {
-                    throw URLError(.badServerResponse)
-                }
-                
-                return output.data
-            }
-            .receive(on: DispatchQueue.main)
+        coinSubscription = NetworkingManager.download(url: API_URL)
             .decode(type: [CoinModel].self, decoder: JSONDecoder())
-            .sink(receiveCompletion: { completion in
-                switch completion {
-                case .failure(let error):
-                    print("Failed to fetch data: \(error)")
-                case .finished:
-                    break
-                }
-            }, receiveValue: { [weak self] returnedCoins in
+            .sink(receiveCompletion: NetworkingManager.handleCompletion, receiveValue: { [weak self] returnedCoins in
                 self?.allCoins = returnedCoins
                 self?.coinSubscription?.cancel()
             })
