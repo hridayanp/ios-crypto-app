fe65ebb2d59006e239343ccb363b7fa4be9aa061
hridayanp <hridayanphukan123@gmail.com>
2025-04-05 12:00:14 +0530

    added coin description in coin details view

--- ios-crypto-app/Core/Detail/Views/DetailView.swift
+++ ios-crypto-app/Core/Detail/Views/DetailView.swift
@@ -7,86 +7,71 @@
 
 import SwiftUI
 
-struct DetailLoadingView:  View {
+struct DetailLoadingView: View {
+    
     @Binding var coin: CoinModel?
     
     var body: some View {
         ZStack {
             if let coin = coin {
-                Text(coin.name)
-            } else {
-                Text("Loading...")
+                DetailView(coin: coin)
             }
         }
-        
     }
-    
     
 }
 
 struct DetailView: View {
     
-    @StateObject var vm: DetailViewModel
-    
+    @StateObject private var vm: DetailViewModel
+    @State private var showFullDescription: Bool = false
     private let columns: [GridItem] = [
         GridItem(.flexible()),
-        GridItem(.flexible())
+        GridItem(.flexible()),
     ]
-    
-    private var spacing: CGFloat = 20
+    private let spacing: CGFloat = 30
     
     init(coin: CoinModel) {
-        
         _vm = StateObject(wrappedValue: DetailViewModel(coin: coin))
-        
-        print("View of coin \(_vm)")
     }
     
     var body: some View {
         ScrollView {
-            
-            
-            
-            VStack(spacing: 20) {
-                
+            VStack {
                 ChartView(coin: vm.coin)
                     .padding(.vertical)
                 
-                VStack(spacing:20) {
-                    
+                VStack(spacing: 20) {
                     overviewTitle
                     Divider()
-                    
-                    overviewLazyGrid
-                    
+                    descriptionSection
+                    overviewGrid
                     additionalTitle
                     Divider()
-                    
-                    additionalLazyGrid
-                    
-                    
-                    
+                    additionalGrid
+                    websiteSection
                 }
                 .padding()
             }
         }
+        .background(
+            Color.theme.background
+                .ignoresSafeArea()
+        )
         .navigationTitle(vm.coin.name)
         .toolbar {
-            ToolbarItem(placement: .topBarTrailing) {
+            ToolbarItem(placement: .navigationBarTrailing) {
                 navigationBarTrailingItems
             }
         }
-        
-        
     }
 }
 
 struct DetailView_Previews: PreviewProvider {
     static var previews: some View {
         NavigationView {
-            DetailView(coin: (developerPreview.coin))
+            DetailView(coin: developerPreview.coin)
         }
-        
     }
 }
 
@@ -110,7 +95,6 @@ extension DetailView {
             .frame(maxWidth: .infinity, alignment: .leading)
     }
     
-    
     private var additionalTitle: some View {
         Text("Additional Details")
             .font(.title)
@@ -119,30 +103,75 @@ extension DetailView {
             .frame(maxWidth: .infinity, alignment: .leading)
     }
     
-    private var overviewLazyGrid: some View {
+    private var descriptionSection: some View {
+        ZStack {
+            if let coinDescription = vm.coinDescription,
+               !coinDescription.isEmpty {
+                VStack(alignment: .leading) {
+                    Text(coinDescription)
+                        .lineLimit(showFullDescription ? nil : 3)
+                        .font(.callout)
+                        .foregroundColor(Color.theme.secondaryText)
+                    
+                    Button(action: {
+                        withAnimation(.easeInOut) {
+                            showFullDescription.toggle()
+                        }
+                    }, label: {
+                        Text(showFullDescription ? "Less" : "Read more..")
+                            .font(.caption)
+                            .fontWeight(.bold)
+                            .padding(.vertical, 4)
+                    })
+                    .accentColor(.blue)
+                }
+                .frame(maxWidth: .infinity, alignment: .leading)
+            }
+        }
+    }
+    
+    private var overviewGrid: some View {
         LazyVGrid(
             columns: columns,
             alignment: .leading,
             spacing: spacing,
-            pinnedViews: []) {
+            pinnedViews: [],
+            content: {
                 ForEach(vm.overviewStatistics) { stat in
                     StatisticView(stat: stat)
-                    
                 }
-            }
+            })
     }
     
-    
-    private var additionalLazyGrid: some View {
+    private var additionalGrid: some View {
         LazyVGrid(
             columns: columns,
             alignment: .leading,
             spacing: spacing,
-            pinnedViews: []) {
+            pinnedViews: [],
+            content: {
                 ForEach(vm.additionalStatistics) { stat in
                     StatisticView(stat: stat)
-                    
                 }
-            }
+            })
     }
+    
+    private var websiteSection: some View {
+        VStack(alignment: .leading, spacing: 20) {
+            if let websiteString = vm.websiteURL,
+               let url = URL(string: websiteString) {
+                Link("Website", destination: url)
+            }
+            
+            if let redditString = vm.redditURL,
+               let url = URL(string: redditString) {
+                Link("Reddit", destination: url)
+            }
+            
+        }
+        .accentColor(.blue)
+        .frame(maxWidth: .infinity, alignment: .leading)
+        .font(.headline)
+    }
+    
 }
